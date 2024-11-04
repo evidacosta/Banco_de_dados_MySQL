e8b691de99677411ec6641b2e55732e56b433cab
evidacosta <costaevida167@gmail.com>
2024-11-04 15:30:51 -0300

    Tab vendas

---
+++ TABVENDAS.sql
@@ -0,0 +1,7 @@
+INSERT INTO vendas(id_venda,produto,categoria,quantidade,
+preco_unitario)VALUES
+(1,'Caderno','Papelaria',10,5.00),
+(2,'Caneta','Papelaria',20,1.50),
+(3,'Lápis','Papelaria',15,1.00),
+(4,'Livro','Livros',5,25.00),
+(5,'Livros de arte','Livros',2,30.00);                  
